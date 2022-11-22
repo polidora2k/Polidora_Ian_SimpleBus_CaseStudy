bce143b9d0076bdbc90dbe4179d0935444faaa5d
Ian Polidora <polidora2k@gmail.com>
2022-11-22 09:42:45 -0500

    Initial Commit

---
+++ src/main/java/org/ianpolidora/simplebus/SimpleBusApplication.java
@@ -0,0 +1,13 @@
+package org.ianpolidora.simplebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBusApplication.class, args);
+	}
+
+}
